1fe3c28a19dfecf2b784624417714ae2fa753fa1
padelagani <padelagani1@gmail.com>
2016-01-14 23:08:47 +0530

    Home Page Setting
    Set Home page and css....

--- 02 Basic Express Website/express_website/app.js
+++ 02 Basic Express Website/express_website/app.js
@@ -6,8 +6,8 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
 
+
 var routes = require('./routes/index');
-var users = require('./routes/users');
 
 //express variable
 var app = express();
@@ -26,7 +26,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 
 app.use('/', routes);
-app.use('/users', users);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
