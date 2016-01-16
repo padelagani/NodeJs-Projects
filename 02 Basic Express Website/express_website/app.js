10ac609e44f170177abab1c0d7f3821b6e983ad4
padelagani <padelagani1@gmail.com>
2016-01-16 12:21:12 +0530

    Node Mailer Done

--- 02 Basic Express Website/express_website/app.js
+++ 02 Basic Express Website/express_website/app.js
@@ -6,7 +6,6 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 var nodemailer = require('nodemailer');
 
-
 var routes = require('./routes/index');
 var about = require('./routes/about');
 var contact = require('./routes/contact');
@@ -25,7 +24,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
 
 app.use('/', routes);
 app.use('/about', about);
